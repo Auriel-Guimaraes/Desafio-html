66d19cb63f9669cc69d203f417e38e0ad3e11702
Auriel-Guimaraes <andrebd15@gmail.com>
2024-07-19 19:08:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Adiciona um event listener ao botão para alternar o modo escuro
+document.getElementById('dark-mode-toggle').addEventListener('click', function() {
+    // Alterna a classe 'dark-mode' no corpo do documento
+    document.body.classList.toggle('dark-mode');
+});
